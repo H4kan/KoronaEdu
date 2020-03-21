61ae9809e658e67a7398ba900c2e8a19eb86e0bf
julkak <62254049+julkak@users.noreply.github.com>
2020-03-21 21:24:37 +0100

    Add files via upload

---
+++ lesson-view.js
@@ -0,0 +1,67 @@
+
+const clickerLifetime = 5000;
+const users = [{user: "Alpha", clicked: 0, missed: 0, points: 0},
+{user: "Beta", clicked: 0, missed: 0, points: 0},
+{user: "Gamma", clicked: 0, missed: 0, points: 0},
+{user: "Delta", clicked: 0, missed: 0, points: 0}];
+
+const studentList = document.querySelectorAll(".userVideo .student");
+
+updateUsers = () => {
+for (let i = 0; i < users.length; i++) {
+    studentList[i].querySelector('.name').textContent = users[i].user;
+    studentList[i].querySelector('.result').textContent = users[i].points;
+    }
+}
+
+const screenEl = document.querySelector(".screen");
+const clicker = document.createElement("div");
+clicker.classList.add("clicker");
+screenEl.appendChild(clicker);
+
+initializeClicker = () => {
+    const firstPos = 50 + Math.random() * (screenEl.offsetHeight * 3 / 4  - 100 - clicker.offsetHeight / 2);
+    const secondPos = 50 + Math.random() * (screenEl.offsetWidth - 100 - clicker.offsetWidth / 2);
+    let clicked = false;
+    clicker.style.top = firstPos + "px";
+    clicker.style.left = secondPos + "px";
+    clicker.classList.remove("clicked")
+    clicker.style.display = "block";
+    setTimeout(() => {
+        if (!clicker.classList.contains("clicked")) {
+            users[0].missed++; 
+            clicker.classList.add("clicked");
+        }
+        else {users[0].clicked++;}
+    }, clickerLifetime);
+}
+
+clickerHandler = () => {
+    clicker.classList.add("clicked");
+}
+
+function get_message(){
+	var nowa = document.getElementById("message").value;
+	return nowa; 
+}
+
+function send_message(message){
+
+	messages.push(message); 
+
+	for (var i = 0; i < messages.length; i++) {
+    
+		document.querySelector(".message"+i).innerHTML = messages[messages.length-i-1];
+	
+	}
+
+	var pole = document.getElementById("message");
+	pole.value="";
+    
+}
+
+const messages = []; 
+
+clicker.addEventListener("click", clickerHandler);
+
+updateUsers();
